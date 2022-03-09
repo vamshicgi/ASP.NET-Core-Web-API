4190f2f888a8a28ff757c3c11b7792c3fb6167e3
CGIVAMSHI <cgi.vamshi@gmail.com>
2022-03-09 13:12:25 +0530

    Enabling Swagger for RESTful APIs expressed using JSON.

--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,6 +26,22 @@ namespace ASP.NETCoreWebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Vamshi .NET Core Web API",
+                    Version = "v1",
+                    Description = "Demo of ASP.NET Core Web API",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Vamshi Krishna Bandi",
+                        Email = string.Empty,
+                        Url = new Uri("http://localhost:5720/"),
+                    },
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,7 +55,17 @@ namespace ASP.NETCoreWebAPI
             app.UseRouting();
 
             app.UseAuthorization();
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Vamshi .NET Core Web API V1");
 
+                // To serve SwaggerUI at application's root page, set the RoutePrefix property to an empty string.
+                c.RoutePrefix = string.Empty;
+            });
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
